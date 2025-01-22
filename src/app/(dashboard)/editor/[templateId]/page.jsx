a2044dee84f62d0c4d18c887a43e9d28b16ce0d5
Gaus Al Munir Tushar <gausalmunirtushar@gmail.com>
2025-01-22 23:54:12 +0600

    feat: enhance editor page layout with sidebar, canvas, and settings components

--- src/app/(dashboard)/editor/[templateId]/page.jsx
+++ src/app/(dashboard)/editor/[templateId]/page.jsx
@@ -1,11 +1,25 @@
+import Canvas from "@/components/editor/Canvas";
 import EditorHeader from "@/components/editor/EditorHeader";
+import EditorSidebar from "@/components/editor/EditorSidebar";
+import Settings from "@/components/editor/Settings";
 import React from "react";
 
 const EditorPage = () => {
 	return (
-		<div>
+		<main>
 			<EditorHeader />
-		</div>
+			<section className="grid grid-cols-5">
+				<div>
+					<EditorSidebar />
+				</div>
+				<div className="col-span-3 bg-gray-100">
+					<Canvas />
+				</div>
+				<div>
+					<Settings />
+				</div>
+			</section>
+		</main>
 	);
 };
 
