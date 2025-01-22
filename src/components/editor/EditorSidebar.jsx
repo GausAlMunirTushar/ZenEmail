8f7a16847afcbbb7e3fbd9ea87ce1d5595378a35
Gaus Al Munir Tushar <gausalmunirtushar@gmail.com>
2025-01-23 00:25:09 +0600

    feat: add tailwind-scrollbar for improved scrollbar styling and enhance editor sidebar with elements section

--- src/components/editor/EditorSidebar.jsx
+++ src/components/editor/EditorSidebar.jsx
@@ -1,18 +1,31 @@
 import Layout from "@/data/layout";
 import React from "react";
 import ElementLayoutCard from "./ElementLayoutCard";
+import ElementList from "@/data/elementList";
 
 const EditorSidebar = () => {
 	return (
-		<aside className="p-4">
-			<div>
-				<h1 className="text-xl font-bold mb-2">Layouts</h1>
-			</div>
-			<div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-				{Layout.map((layout, index) => (
-					<ElementLayoutCard layout={layout} key={index} />
-				))}
-			</div>
+		<aside className="p-4 h-screen overflow-y-auto scrollbar-none">
+			<section>
+				<div>
+					<h1 className="text-xl font-bold mb-2">Layouts</h1>
+				</div>
+				<div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+					{Layout.map((layout, index) => (
+						<ElementLayoutCard layout={layout} key={index} />
+					))}
+				</div>
+			</section>
+			<section>
+				<div>
+					<h1 className="text-xl font-bold my-2">Elements</h1>
+				</div>
+				<div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+					{ElementList.map((element, index) => (
+						<ElementLayoutCard layout={element} key={index} />
+					))}
+				</div>
+			</section>
 		</aside>
 	);
 };
